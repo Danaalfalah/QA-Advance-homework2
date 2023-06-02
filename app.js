fa115fa7e3eb7f3df54121d68ae3f908725b2f7d
Mahmoud Ismail <alfalahdana3@gmail.com>
2023-06-02 17:49:20 +0400

    homework

---
+++ app.js
@@ -0,0 +1,27 @@
+let studentsName=["Mahmoud","Dana","Zaid","Esmael"]
+let studentsMarks=[43,80,66,33]
+
+function StudentsMarks(params) {
+
+    for (let i = 0; i< studentsName.length; i++) {
+
+      for (let k = 0; k <studentsName.length; k++) {
+       if (studentsMarks[k]>=50 && i==k) {
+        console.log("Congrats "+studentsName[i]+" your score : " +studentsMarks[k]+" and you have passed 💖")
+        continue;
+       }
+       else if (studentsMarks[k]<=50 && i==k){
+        console.log("Sorry "+studentsName[i]+" your score : " +studentsMarks[k]+" and you have failed 😢 ")
+        
+       }
+      
+        
+      }
+        
+    }
+    
+}
+
+StudentsMarks()
+
+
